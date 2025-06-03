e8e5690bc8734cdf44dc7fe5ef1a36f83b9a3d20
Oscar Hernandez S. <30444713+Oscarhs117@users.noreply.github.com>
2025-06-03 15:07:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,6 @@ def handle_file():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
